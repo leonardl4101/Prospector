21513944306b477542be1a88094548ccd25548b7
Leonard Leyva <ll788@scarletmail.rutgers.edu>
2019-11-07 15:41:03 -0500

    continue

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -3,53 +3,86 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
+	public string           suit; 
+	public int              rank;
+	public Color            color = Color.black;
+	public string           colS = "Black";
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
 	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
+	public List<GameObject> pipGOs  = new List<GameObject>();
+	public GameObject       back;
 	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
-
+	public CardDefinition   def;
+	
+	public SpriteRenderer[]        spriteRenderers;
+	
+	void Start() {
+		SetSortOrder(0);
+	}
 
 	public bool faceUp {
 		get {
-			return (!back.activeSelf);
+			return( !back.activeSelf );
 		}
-
 		set {
 			back.SetActive(!value);
 		}
 	}
 
+	public void PopulateSpriteRenderers() {
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
 
-	// Use this for initialization
-	void Start () {
-	
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers();
+		
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd) {
+				PopulateSpriteRenderers ();
+				foreach (SpriteRenderer tSR in spriteRenderers) {
+						if (tSR.gameObject == this.gameObject) {
+								tSR.sortingOrder = sOrd;
+								continue;
+				
+						}
+						switch (tSR.gameObject.name) {
+						case "back": // if the name is "back"
+								tSR.sortingOrder = sOrd + 2;
+								break;
+						case "face":
+						default:
+								tSR.sortingOrder = sOrd + 1;
+								break;
+						}
+				}
+		}
+
+	virtual public void OnMouseUpAsButton() {
+		print (name);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
-
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+public class Decorator {
+	
+	public string    type; 
+	public Vector3   loc; 
+	public bool      flip = false; 
+	public float     scale = 1f;  
+}
+
+[System.Serializable]
+public class CardDefinition {
+	
+	public string             face; // Sprite to use for each face card
+	public int                rank; // The rank (1-13) of this card
+	public List<Decorator>    pips = new List<Decorator>();  
+	
 }
--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -34,14 +34,14 @@ public class Utils : MonoBehaviour {
 		// Create an empty Bounds b
 		Bounds b = new Bounds(Vector3.zero, Vector3.zero);
 		// If this GameObject has a Renderer Component...
-		if (go.GetComponent<Renderer>() != null) {
+		if (go.renderer != null) {
 			// Expand b to contain the Renderer's Bounds
-			b = BoundsUnion(b, go.GetComponent<Renderer>().bounds);
+			b = BoundsUnion(b, go.renderer.bounds);
 		}
 		// If this GameObject has a Collider Component...
-		if (go.GetComponent<Collider>() != null) {
+		if (go.collider != null) {
 			// Expand b to contain the Collider's Bounds
-			b = BoundsUnion(b, go.GetComponent<Collider>().bounds);
+			b = BoundsUnion(b, go.collider.bounds);
 		}
 		// Iterate through each child of this gameObject.transform
 		foreach( Transform t in go.transform ) {
@@ -222,8 +222,8 @@ public class Utils : MonoBehaviour {
 	// Returns a list of all Materials in this GameObject or its children
 	static public Material[] GetAllMaterials( GameObject go ) {
 		List<Material> mats = new List<Material>();
-		if (go.GetComponent<Renderer>() != null) {
-			mats.Add(go.GetComponent<Renderer>().material);
+		if (go.renderer != null) {
+			mats.Add(go.renderer.material);
 		}
 		foreach( Transform t in go.transform ) {
 			mats.AddRange( GetAllMaterials( t.gameObject ) );
